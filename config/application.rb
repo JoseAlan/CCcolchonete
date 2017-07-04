e7e02baf84f4cf6511d8c9c20bf0b773e038539c
vagrant <alan123art@gmail.com>
2017-07-04 23:36:17 +0000

    Capitulo 6 e 7, Css e alterando idioma da aplicacao

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,19 @@ module Colchonet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    # Set Time.zone default to the specified zone
+	# and make Active Record auto-convert to this zone.
+	# Run "rake -D time" for a list of
+	# tasks for finding time zone names. Default is UTC.
+	config.time_zone = 'Brasilia'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # The default locale is :en and all
+	# translations from config/locales/*.rb,yml are auto loaded.
+	# Rails.application.config.i18n.load_path
+	# += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+	config.i18n.default_locale = :pt
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
+  scope ":locale", locate: /en|pt/ do
+	resources :rooms
+	resources :users
+   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  get '/:locale' => 'home#index', locale: /en|pt/
+  root "home#index"
+
 end
