7ea639a2f2292015f1883c75030dfb8f0239d234
vagrant <alan123art@gmail.com>
2017-07-12 00:39:56 +0000

    Capitulo 9 completo

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,11 +1,19 @@
 class UserSessionsController < ApplicationController
 	def new
-	@user_session = UserSession.new
+		@user_session = UserSession.new(session)
 	end
+
 	def create
-	# Ainda não :-)
+			@user_session = UserSession.new(session,
+			params[:user_session])
+		if @user_session.authenticate!
+		# Não esqueça de adicionar a chave no i18n!
+		redirect_to root_path, notice: t('flash.notice.signed_in')
+		else
+		render :new
+		end
 	end
+	
 	def destroy
-	# Ainda não :-)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
 	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
+	scope :confirmed, -> { where.not(confirmed_at: nil) }
+
+	
+
 	validates_presence_of :email, :full_name, :location
 	validates_length_of :bio, minimum: 30, allow_blank: false
 	validates_uniqueness_of :email
@@ -11,4 +15,11 @@ class User < ApplicationRecord
 	end
 
 	has_secure_password
+
+	
+  	def self.authenticate(email, password)
+    confirmed.
+      find_by(email: email).
+      try(:authenticate, password)
+  	end
 end
