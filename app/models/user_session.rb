ac802b38cd658150839f6b253dfc81641ee4c97c
vagrant <alan123art@gmail.com>
2017-07-28 00:45:00 +0000

    Capitulo 10 Controle de acesso

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -11,16 +11,28 @@ class UserSession
 		
 		def authenticate!
 			user = User.authenticate(@email, @password)
-			if user.present?
-				store(user)
-			else
-				errors.add(:base, :invalid_login)
-				false
-			end
+				if user.present?
+					store(user)
+				else
+					errors.add(:base, :invalid_login)
+					false
+				end
 		end
 		
 		def store(user)
 			@session[:user_id] = user.id
+		end
+
+		def current_user
+			User.find(@session[:user_id])
+		end
+
+		def user_signed_in?
+			@session[:user_id].present?
+		end
+
+		def destroy
+			@session[:user_id] = nil
 		end
 end
 
